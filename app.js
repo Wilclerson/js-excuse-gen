f7b75785c057666811869ba1907c1386a8c9c457
wraymond2017 <wraymond2017@outlook.com>
2024-01-31 21:15:21 -0500

    Final Push

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const generateExcuse = () => {
-    const firstNameArray = ['Travis', 'Robert', 'Alfredo', 'Greg', 'Stan']
-    const lastNameArray = ['Macero', 'Howell', 'Gonzalez', 'Lin']
+
+    const who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+    const action = ['ate', 'peed', 'crushed', 'broke'];
+    const what = ['my homework', 'the keys', 'the car'];
+    const when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
     
-    const firstNameIndex = Math.floor(Math.random() * firstNameArray.length)
-    const lastNameIndex = Math.floor(Math.random() * lastNameArray.length)
+    const whoIndex = Math.floor(Math.random() * who.length);
+    const actionIndex = Math.floor(Math.random() * action.length);
+    const whatIndex = Math.floor(Math.random() * what.length);
+    const whenIndex = Math.floor(Math.random() * when.length);
+
+    const text = (`Hello ${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`);
     
-    console.log(`Hello ${firstNameArray[firstNameIndex]} ${lastNameArray[lastNameIndex]}`)
-  }
-  
-  generateExcuse()
+    document.getElementById("excuse").innerHTML = text;
+
+}
+
   
